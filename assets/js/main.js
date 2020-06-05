828e4f72c9fbcd175dec7bb0b80e11459e4d11f8
Gnanavel76 <49830744+Gnanavel76@users.noreply.github.com>
2020-06-05 17:54:54 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+ $(document).ready(function(){
+        $('header').css('height',$(window).innerHeight()+'px')
+        $(window).on('load',function(){
+                $('#preloader').fadeOut(1000);
+        });
+        let burger = $('.toggle_icon');
+        let navtab = $('header');
+        let dh = $(document).height();
+        let wh = $(window).height();
+        // Function to add active class to navigation link while scrolling
+        function whenScroll(){
+                $('.navlink a').each(function(){
+                        let scroll = $(window).scrollTop();
+                        if(scroll >= $($(this).attr('href')).offset().top - 300 || $($(this).attr('href')).offset().top - 300 >= $(window).pageYOffset){
+                                $('.navlink a').removeClass('active');
+                                $('.navlink span').removeClass('active');
+                                $(this).addClass('active');
+                                $(this).siblings().addClass('active');
+                                let value = (scroll / (dh - wh)) * 100;
+                                $('#progress').css('height', value + '%');
+                        }
+                });
+        }
+
+     // Function to add animation to Skills section
+     function skillAnim(){
+        // Adding animation to Skills section
+        let scroll = $(window).scrollTop();
+        if(scroll >= $('#skills').offset().top - 300){
+                let selector = document.querySelectorAll('#skills .container .skillBx .imgBx');
+                selector.forEach((link) => {
+                        link.style.animation = 'swim 10s ease-in-out forwards,slide 400s linear infinite 9.7s forwards';
+                })
+        }
+     }
+
+     // To add active class to navigation link while page reloads
+     whenScroll();
+
+     // To add animation to skill section when page reloads
+     skillAnim();
+
+     // Progrss bar
+     $(window).on('scroll',function(){
+        let scroll = $(window).scrollTop();
+        let value = (scroll / (dh - wh)) * 100;
+        $('#progress').css('height', value + '%');
+
+        // To add animation to skill section when page scolls
+        skillAnim();
+        // To add active class to navigation link while scrolling
+        whenScroll();
+     });
+
+     // Typed.js
+     new Typed('#typed',{
+        strings: ['Web Developer','Freelancer'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+     });
+
+     // Changing avatars
+     let circle = $('.round');
+     let avatars = $('.avatars');
+     let i = 1;
+     let imageArray = ['assets/images/avatar.jpg','assets/images/avatar2.jpg','assets/images/avatar3.jpg']
+     circle.on('animationend',function(){
+         $(this).css('animation','none');
+         avatars.attr('src',imageArray[i]);
+         $(this).css('animation');
+         $(this).css('animation','fill 5s linear 4s');
+         if(i < imageArray.length - 1){
+             i++;
+         }
+         else{
+             i=0;
+         }
+     });
+
+        // Navbar Toggle Icon
+     burger.click(function(){
+         navtab.toggleClass('active');
+         burger.toggleClass('toggle_icon1');
+     });
+
+     $('.navlink a').click(function(){
+        if($(window).width() <= 1080){
+                navtab.toggleClass('active');
+                if(burger.hasClass('toggle_icon1')){
+                        burger.toggleClass('toggle_icon1');
+                }
+        }
+})
+
+});
